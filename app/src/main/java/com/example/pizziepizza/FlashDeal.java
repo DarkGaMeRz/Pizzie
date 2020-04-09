53d7001332a96c69c64620dd0c18ecdf1ae7f81d
DarkGaMeRz <Thanathornb@gmail.com>
2020-04-09 10:47:01 +0700

    1st version-incomplete

---
+++ app/src/main/java/com/example/pizziepizza/FlashDeal.java
@@ -0,0 +1,11 @@
+package com.example.pizziepizza;
+
+import android.os.Bundle;
+
+public class FlashDeal extends Home {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashdeal);
+    }
+}
---
+++ app/src/main/java/com/example/pizziepizza/Home.java
@@ -0,0 +1,55 @@
+package com.example.pizziepizza;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button fDB = (Button)findViewById(R.id.button);
+        Button cSB = (Button)findViewById(R.id.button2);
+        Button pMB = (Button)findViewById(R.id.button3);
+        Button oB = (Button)findViewById(R.id.button4);
+
+        fDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int1 = new Intent(Home.this,FlashDeal.class);
+                startActivity(int1);
+            }
+        });
+
+        cSB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int2 = new Intent(Home.this,FlashDeal.class);
+                startActivity(int2);
+            }
+        });
+
+        pMB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int3 = new Intent(Home.this,FlashDeal.class);
+                startActivity(int3);
+            }
+        });
+
+        oB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int4 = new Intent(Home.this,FlashDeal.class);
+                startActivity(int4);
+            }
+        });
+    }
+
+}
