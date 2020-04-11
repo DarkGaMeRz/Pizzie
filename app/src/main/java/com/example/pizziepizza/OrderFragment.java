a7dae4d27c07f0e1591ac7280357e87141f8c309
NiNickel <nichaphat09.tonliw@gmail.com>
2020-04-12 01:19:32 +0700

    edit BottomNav style and ActionBar style. App has stopped fixed

--- app/src/main/java/com/example/pizziepizza/OrderFragment.java
+++ app/src/main/java/com/example/pizziepizza/OrderFragment.java
@@ -11,24 +11,12 @@ import androidx.fragment.app.Fragment;
 
 public class OrderFragment extends Fragment {
 
-    //private HomeViewModel homeViewModel;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
-/*
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
- */
         return inflater.inflate(R.layout.fragment_order,container,false);
+
     }
 }
